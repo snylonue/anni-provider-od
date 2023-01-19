dd027da960f9299e9d3ec62e880bb5e92bebe018
snylonue <snylonue@gmail.com>
2023-01-19 10:06:46 +0800

    chore: minor simplification

--- src/lib.rs
+++ src/lib.rs
@@ -126,9 +126,7 @@ impl AnniProvider for OneDriveProvider {
         let reader = StreamReader::new(resp.bytes_stream().map(|result| {
             result.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))
         }));
-        let (duration, reader) = info::read_duration(Box::pin(reader), range)
-            .await
-            .map_err(ProviderError::FlacError)?;
+        let (duration, reader) = info::read_duration(Box::pin(reader), range).await?;
         Ok(AudioResourceReader {
             info: AudioInfo {
                 extension: String::from("flac"),
